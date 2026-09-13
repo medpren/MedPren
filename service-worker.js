aca33ab03c123347f8ccac3f1d8b21a11c3b8d3a
medpren <baobabsintellectuels@gmail.com>
2026-09-13 23:10:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mmedpren-v3-3-20260913';
+const CACHE_NAME = 'mmedpren-v3-4-20260913';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
